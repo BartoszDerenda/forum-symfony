e6963cf1d724d73a4dfe4617e7a2139b325d2294
BartoszDerenda <bartoszderenda00@gmail.com>
2022-08-19 12:44:46 +0200

    push 14
    Login + registration implementation.
    Fixed issue with empty images on posts without images.

---
+++ src/Form/Type/RegistrationType.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Registration type.
+ */
+
+namespace App\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+/**
+ * Class TaskType.
+ */
+class RegistrationType extends AbstractType
+{
+    /**
+     * Builds the form.
+     *
+     * This method is called for each type in the hierarchy starting from the
+     * top most type. Type extensions can further modify the form.
+     *
+     * @param FormBuilderInterface $builder The form builder
+     * @param array<string, mixed> $options Form options
+     *
+     * @see FormTypeExtensionInterface::buildForm()
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+            $builder->add('email');
+            $builder->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'required' => true,
+                'first_options' => ['label' => 'Password'],
+                'second_options' => ['label' => 'Repeat password'],
+            ]);
+            $builder->add('register', SubmitType::class, [
+                'attr' => [
+                    'class' => 'btn btn-success'
+                ]
+            ]);
+    }
+}
